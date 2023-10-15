0f45a71cf6901893e49e481e5d794cd5df0fee55
AchRef <rahaliashraf732@gmail.com>
2023-10-16 00:17:33 +0100

    hello

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,15 +1,37 @@
 from currency_converter import CurrencyConverter
 import pycountry
+import time
 
-# Create an instance of the CurrencyConverter class
 c = CurrencyConverter()
 i = 0
 
-# Get the list of all available currencies
 all_currencies = c.currencies
 
-# Print the list of currencies
-print("pick a currency you want to convert :")
+currency_dict = {}
+
+print("Here are our currencies:")
 for currency in all_currencies:
     i += 1
     print(f"{i}- {currency}")
+    currency_dict[i] = currency
+
+amount = float(input("pick the amount: "))
+currency_one = int(input("pick a currency you want to convert: "))
+print(f"{amount} {currency_dict[currency_one]}")
+
+currency_two = int(input("pick a currency you want to convert: "))
+print(currency_dict[currency_two])
+
+print(
+    f"converting {amount} {currency_dict[currency_one]} to {currency_dict[currency_two]}"
+)
+
+time.sleep(3)
+
+converted_amount = c.convert(
+    amount, currency_dict[currency_one], currency_dict[currency_two]
+)
+
+print(
+    f"{amount} {currency_dict[currency_one]} = {round(converted_amount, 2)} {currency_dict[currency_two]}"
+)
